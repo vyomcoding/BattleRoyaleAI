57df802522f1091c31425563cd62a06df5502d67
vyomcoding <jaidev.janardana@gmail.com>
2021-02-06 17:12:57 +0000

    We found out what the extreme left actor and extreme right \n and extreme down and extreme up

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 class Universe {
 	constructor(){
 		this.offset_x = 0
@@ -10,10 +9,39 @@ class Universe {
 
 		this.actors = []
 		this.stage = null
+
+		this.leftActor =   9999999999999
+		this.rightActor = -9999999999999
+		this.upActor =     9999999999999
+		this.downActor =  -9999999999999
+	}
+
+
+  actorLimits() {
+		// this.actors = [class, class, ...]
+    // class = {'x':12, 'y':23, ...}
+    
+		let actorsX = []
+		let actorsY = []
+  
+		for (i=0;i<this.actors.length;i++){
+      actorsX.push(this.actors[i].x)
+    }
+
+		leftActor = Math.min(actorsX)
+		rightActor = Math.max(actorsX)
+
+		for (i=0;i<this.actors.length;i++){
+      actorsY.push(this.actors[i].y)
+    }
+
+		upActor = Math.min(actorsY)
+		downActor = Math.max(actorsY)
+		
 	}
 
 	rescaleBackground(){
-    
+
 	}
 
 	repositionActors(){
