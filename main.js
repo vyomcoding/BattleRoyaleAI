888db4059fe1dded8945b282030d590026f03215
vyomcoding <jaidev.janardana@gmail.com>
2021-02-19 14:10:42 +0000

    the lesson

--- main.js
+++ main.js
@@ -7,20 +7,13 @@ class Universe {
 		this.height = 400
 		this.width = 400
 
-		this.actors = []
+		this.actors = null
 		this.stage = null
-
-		this.leftActor =   9999999999999
-		this.rightActor = -9999999999999
-		this.upActor =     9999999999999
-		this.downActor =  -9999999999999
 	}
 
 
   actorLimits() {
-		// this.actors = [class, class, ...]
-    // class = {'x':12, 'y':23, ...}
-    
+		
 		let actorsX = []
 		let actorsY = []
   
@@ -28,16 +21,20 @@ class Universe {
       actorsX.push(this.actors[i].x)
     }
 
-		leftActor = Math.min(actorsX)
-		rightActor = Math.max(actorsX)
+		let leftActor = Math.min(...actorsX)
+		let rightActor = Math.max(...actorsX)
 
 		for (i=0;i<this.actors.length;i++){
       actorsY.push(this.actors[i].y)
     }
 
-		upActor = Math.min(actorsY)
-		downActor = Math.max(actorsY)
+
+		let upActor = Math.min(...actorsY)
+		let downActor = Math.max(...actorsY)
+    
+    console.log(upActor)
 		
+    return([upActor, downActor, leftActor, rightActor])
 	}
 
 	rescaleBackground(){
--- script.js
+++ script.js
@@ -1,195 +1,71 @@
-
-//vars
-	var H = 400
-	var W = 400 
-
-	var res_x = 10
-	var res_y = 10
-
-	var zoom_x = 1
-	var zoom_y = 1
-
-	var offset_x = 0
-	var offset_y = 0
-
-	var alivePreys = []
-	var alivePredators = []
-
-	var predators = []
-	var preys = []
-
-	var x = []
-	var y = []
-
-	var fleeDirection = 0
-	var timeAlive = 0
-
-	var line = {
-		x : x,
-		y : y,// big brain
-		mode: 'markers'
+class Circle {
+	constructor(){
+		this.r = 10
+		this.x = 200
+		this.y = 200
+		this.d = this.r*2
 	}
 
-	var layout = {
-		margin: {
-			l: 40,
-			r: 20,
-			b: 30,
-			t: 10,
-			pad: 4
-		}
+	draw(){
+		fill(0)
+		ellipse(this.x, this.y, this.d, this.d)
 	}
 
-//
-
-data = [line]
-Plotly.newPlot('plot', data, layout, {staticPlot: false})
-               
-function restart() {
-  fleeDirection = Math.random()*PI*2
-  timeAlive = 0
-
-  alivePreys = []
-  alivePredators = []
-
-  predators = []
-  for (var i = 0; i < 1; i++){
-    AI_ = new ObjectAI()
-    AI_.strategy = 'predator'
-    AI_.d /= 2
-    AI_.topSpeed *= .4
-    AI_.strength *= 3
-    predators.push(AI_)
-  }
-
-  preys = []
-  for (var i = 0; i < 1; i++){
-    AI_ = new ObjectAI()
-    AI_.strategy = 'prey'
-    AI_.topSpeed *= .8
-    AI_.weaponDistance *= 1.5
-    preys.push(AI_)
-  }
+	move(){
+		this.x += (Math.random() - 0.5) * 5
+		this.y += (Math.random() - 0.5 )* 5
+	}
 }
 
-function gameOver(){
-  x.push(fleeDirection)
-  y.push(timeAlive)
+var multiverse = new Universe()
 
-  Plotly.redraw('plot')
+var circles = []
+
+for (i=0;i<5;i++) {
+	var circ_ = new Circle()
+
+	circles.push(circ_)
 }
 
 function setup(){
-  restart()
-  var myCanvas = createCanvas(W, H)
-  myCanvas.parent('canvas')
+	createCanvas(400, 400)
 }
 
-function reCentre(){
-  let midpoint = _midpoint( preys[0], predators[0])
+
+
+function back_() {
+	for (i=0;i<40;i++) {
+    
+		line(i*10, 0, i*10, 400)
+		line(0, i*10, 400, i*10)
+	}
+}
+
+
+
+function draw(){
+
+
+
+	background(200)
 	
-  translate(200-midpoint.x, 200-midpoint.y)
-}
-
-function reScale(){
-  let dist_x = Math.abs(preys[0].x - predators[0].x)
-  let dist_y = Math.abs(preys[0].y - predators[0].y)
-
-	if (dist_x>W) {
-		zoom_x /= 1.1
+	for (i=0;i<5;i++) {
+		circles[i].draw()
+		circles[i].move()
 	}
 
-	if (dist_y>H){
-		zoom_y/= 1.1
-	}
+	back_()
 
-
+	multiverse.actors = circles
+	let limits = multiverse.actorLimits()
+  push()
+  stroke(0, 255, 0)
+  noFill()
+	strokeWeight(7)
+	rect(limits[2], limits[0], limits[3]-limits[2], limits[1]- limits[0] )
+	pop()
 }
 
-function _background() {
-  
-  let midpoint = _midpoint(preys[0], predators[0])
 
-  offset_x = (midpoint.x - 200)/res_x
-  offset_y = (midpoint.y - 200)/res_y
-  
-  background(200)
-	for (i=0; i<W/res_x; i++) {
-    for (j=0; j<H/res_y; j++){
 
-			fill(0, 255*noise(i/zoom_x/10 + offset_x, j/zoom_y/10 + offset_y, 100),0)
-    	
-      rect(midpoint.x - 200 + res_x*i, midpoint.y-200+res_y*j, res_x, res_y)
-	  }
 
-	}
-}
-
-function draw() {
-  
-  reCentre()
-  reScale()
-  _background()
-
-  alivePredators = []
-  for (var i = 0; i < predators.length; i++){
-    AI_ = predators[i]
-    if (AI_.health <0){
-      continue
-    } else {
-      alivePredators.push(AI_)
-    }
-		
-    AI_.draw()
-		if (AI_.health < 50) {
-    	AI_.move(preys)
-		} else {
-			AI_.move([...predators, ...preys])
-		}
-  } // what was the homework?
-  predators = alivePredators
-
-  alivePreys = []
-  for (var i = 0; i < preys.length; i++){
-    AI_ = preys[i]
-    if (AI_.health <0){
-      continue
-    } else {
-      alivePreys.push(AI_)
-    }
-    AI_.draw()
-    AI_.move(predators)
-  }
-  preys = alivePreys
-
-  fill(255,5,0)
-
-  if (predators.length == 0 || timeAlive>300){
-    gameOver()
-    restart()
-  }
-
-  if (preys.length ==0){
-    gameOver()
-    restart()
-  }
-
-  timeAlive++
-}
-
-function _midpoint(point_1, point_2){
-  
-  let midpoint = {}
-
-  /*
-  point_1 = {'x':23, 'y':43}
-	average between point_1 and point_2
-
-	((point_1.x+point_2.x)/2, (point_1.y+point_2.y)/2)
-	*/
-  
-  midpoint.x = (point_1.x + point_2.x) / 2
-  midpoint.y = (point_1.y + point_2.y) / 2
-
-  return(midpoint)
-}
